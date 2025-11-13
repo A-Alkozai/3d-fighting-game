0560683251fc57638b6bb3cbea587a98e8150629
Alkozai Ahad (2023) ZMAC392 <ahad.alkozai.2023@live.rhul.ac.uk>
2025-11-13 23:01:08 +0000

    No longer contains tree information #9

--- product/Assets/Project/Scripts/Systems/Moves/MoveData.cs
+++ product/Assets/Project/Scripts/Systems/Moves/MoveData.cs
@@ -8,68 +8,34 @@ public class MoveData : IIdentifiable
     [SerializeField] private string id;
     [SerializeField] private string moveName;
     [SerializeField] private string description;
-
     [SerializeField] private int startupFrames;
     [SerializeField] private int activeFrames;
     [SerializeField] private int recoveryFrames;
     [SerializeField] private int totalFrames;
-
     [SerializeField] private int inputDelay;
     [SerializeField] private int branchDelay;
-
     [SerializeField] private List<string> inputSequence;
-    [SerializeField] private List<string> branchMoves;
     [SerializeField] private List<string> requiredStates;
 
     private List<InputCommand> inputSequenceObj = new List<InputCommand>();
     private List<PlayerStates> requiredStatesObj = new List<PlayerStates>();
 
-    private MoveData prevMove;
-    private Dictionary<string, MoveData> nextMoves = new Dictionary<string, MoveData>();
-
     public string Id => id;
     public string MoveName => moveName;
     public string Description => description;
-
     public int StartupFrames => startupFrames;
     public int ActiveFrames => activeFrames;
     public int RecoveryFrames => recoveryFrames;
     public int TotalFrames => totalFrames;
-
     public int InputDelay => inputDelay;
     public int BranchDelay => branchDelay;
-
     public IReadOnlyList<InputCommand> InputSequence => inputSequenceObj;
     public IReadOnlyList<PlayerStates> RequiredStates => requiredStatesObj;
-    public MoveData PrevMove => prevMove;
-    public IReadOnlyDictionary<string, MoveData> NextMoves => nextMoves;
 
-    internal void SetPrevMove(MoveData move)
+    public void InitialiseObjects()
     {
-        prevMove = move;
-    }
-
-    public void InitialiseObjects(Dictionary<string, MoveData> dict)
-    {
-        InitialiseBranches(dict);
         InitialiseInputs();
         InitialiseStates();
-    }
-
-    private void InitialiseBranches(Dictionary<string, MoveData> dict)
-    {
-        foreach (string branchId in branchMoves)
-        {
-            if (dict.TryGetValue(branchId, out MoveData branchMove))
-            {
-                nextMoves[branchId] = branchMove;
-                branchMove.SetPrevMove(this);
-            }
-            else
-            {
-                Debug.LogWarning($"Move '{Id}' contains unknown branch '{branchId}'");
-            }
-        }
     }
 
     private void InitialiseInputs()
