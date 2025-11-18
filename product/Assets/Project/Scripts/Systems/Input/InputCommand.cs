44d387b29ba13f8a0221a6ed1209acd865fc8078
Alkozai Ahad (2023) ZMAC392 <ahad.alkozai.2023@live.rhul.ac.uk>
2025-11-18 21:54:52 +0000

    Added holding down input commands + normalised input commands #7

--- product/Assets/Project/Scripts/Systems/Input/InputCommand.cs
+++ product/Assets/Project/Scripts/Systems/Input/InputCommand.cs
@@ -1,9 +1,18 @@
 public enum InputCommand
 {
     Left,
+    LeftHold,
     Right,
+    RightHold,
+
+    Forward,
+    ForwardHold,
+    Backward,
+    BackwardHold,
     Up,
+    UpHold,
     Down,
+    DownHold,
     LeftPunch,
     RightPunch,
     LeftKick,
