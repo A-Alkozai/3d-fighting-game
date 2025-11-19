f838d20f1296d430fdbcee76e441a45e76316b48
Alkozai Ahad (2023) ZMAC392 <ahad.alkozai.2023@live.rhul.ac.uk>
2025-11-19 23:37:18 +0000

    Modified to detect single tap and held down inputs #7

--- product/Assets/Project/Scripts/Systems/Input/LocalInputProvider.cs
+++ product/Assets/Project/Scripts/Systems/Input/LocalInputProvider.cs
@@ -6,6 +6,11 @@ using System.Collections.Generic;
 public class LocalInputProvider : IInputProvider
 {
     private InputKeys inputKeys;
+    private int holdThreshold = 60;
+    private Dictionary<InputCommand, int> canHoldInput = new Dictionary<InputCommand, int>
+        { {InputCommand.Up, 0}, {InputCommand.Down, 0},
+          {InputCommand.Left, 0}, {InputCommand.Right, 0}};
+    private Dictionary<InputCommand, InputObject> heldDownInputs = new Dictionary<InputCommand, InputObject>();
 
     public LocalInputProvider(InputKeys inputKeys)
     {
@@ -18,9 +23,50 @@ public class LocalInputProvider : IInputProvider
 
         foreach (InputCommand command in Enum.GetValues(typeof(InputCommand)))
         {
-            Key key = inputKeys.keybinds[command];
+            Key key;
+            if (inputKeys.keybinds.ContainsKey(command))
+            {
+                key = inputKeys.keybinds[command];
+            }
+            else continue;
 
-            if (Keyboard.current[key].wasPressedThisFrame)
+            if (canHoldInput.ContainsKey(command))
+            {
+                var name = command.ToString() + "Hold";
+                InputCommand holdName = Enum.Parse<InputCommand>(name);
+
+                if (!Keyboard.current[key].isPressed && canHoldInput[command] == 0) continue;
+                if (!Keyboard.current[key].isPressed && canHoldInput[command] < holdThreshold)
+                {
+                    inputs.Add(new InputObject(command, key));
+                    canHoldInput[command] = 0;
+                }
+                else if (!Keyboard.current[key].isPressed)
+                {
+                    canHoldInput[command] = 0;
+                    heldDownInputs[holdName].GetFrame().DisableFrame();
+                    inputs.Add(heldDownInputs[holdName]);
+                    heldDownInputs.Remove(holdName);
+                }
+
+                if (Keyboard.current[key].isPressed)
+                {
+                    canHoldInput[command]++;
+                }
+
+                if (canHoldInput[command] == holdThreshold)
+                {
+                    InputObject heldInput = new InputObject(holdName, key, true);
+                    heldDownInputs[holdName] = heldInput;
+                    inputs.Add(heldInput);
+                }
+                
+                else if (canHoldInput[command] > holdThreshold)
+                {
+                    heldDownInputs[holdName].GetFrame().UpdateFrame();
+                }
+            }
+            else if (Keyboard.current[key].wasPressedThisFrame)
             {
                 inputs.Add(new InputObject(command, key));
             }
