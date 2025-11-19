6030bac2c22282c97a42bc986b145561772152b6
Alkozai Ahad (2023) ZMAC392 <ahad.alkozai.2023@live.rhul.ac.uk>
2025-11-19 23:41:48 +0000

    Integrated Input Interpreter into Player #7

--- product/Assets/Project/Scripts/Systems/Player/Player.cs
+++ product/Assets/Project/Scripts/Systems/Player/Player.cs
@@ -3,15 +3,30 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private InputBuffer inputBuffer = new InputBuffer();
+    private MovesManager movesManager = new MovesManager();
+    private StateManager stateManager = new StateManager();
+    private InputInterpreter inputInterpreter;
 
     public InputBuffer GetInputBuffer()
     {
         return inputBuffer;
     }
 
+    public InputInterpreter GetInputInterpreter()
+    {
+        return inputInterpreter;
+    }
+
+    public void start()
+    {
+        movesManager.LoadMoves();
+        inputInterpreter = new InputInterpreter(inputBuffer, movesManager.GetMovesDatabase(), stateManager);
+    }
+
     public void update()
     {
         inputBuffer.UpdateFrameCounter();
         inputBuffer.RemoveExpiredInputs();
+        inputInterpreter.Update();
     }
 }
