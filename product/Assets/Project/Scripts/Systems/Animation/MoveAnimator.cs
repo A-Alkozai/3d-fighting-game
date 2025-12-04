561c08ed20864a3feb112061a88dedd9f292f3f4
Alkozai Ahad (2023) ZMAC392 <ahad.alkozai.2023@live.rhul.ac.uk>
2025-12-04 14:32:15 +0000

    Implemented MoveAnimator to play animations in animation controller #11

---
+++ product/Assets/Project/Scripts/Systems/Animation/MoveAnimator.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MoveAnimator : MonoBehaviour
+{
+    private Animator animator;
+    private AnimatorOverrideController overrideController;
+    private string currentAnimation = "Idle";
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        overrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        animator.runtimeAnimatorController = overrideController;
+    }
+
+    public void PlayAnimation(string clipName, float crossfade = 0.1f)
+    {
+        if (currentAnimation != clipName)
+        {
+            currentAnimation = clipName;
+            animator.CrossFade(clipName, crossfade);
+        }
+    }
+}
