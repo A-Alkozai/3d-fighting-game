f591d9c65782c35c7ed172fe97c4b7add383d085
Alkozai Ahad (2023) ZMAC392 <ahad.alkozai.2023@live.rhul.ac.uk>
2026-02-18 16:50:55 +0000

    Integrated Collision System into current system #15

--- product/Assets/Project/Scripts/Systems/Player/Player.cs
+++ product/Assets/Project/Scripts/Systems/Player/Player.cs
@@ -1,26 +1,42 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, ICollidable
 {
+    [Header("Bone References")]
+    [SerializeField] private Transform body;
+    [SerializeField] private Transform headBone;
+    [SerializeField] private Transform bodyBone;
+    [SerializeField] private Transform rightArmUpperBone;
+    [SerializeField] private Transform rightArmLowerBone;
+    [SerializeField] private Transform rightFistBone;
+    [SerializeField] private Transform leftArmUpperBone;
+    [SerializeField] private Transform leftArmLowerBone;
+    [SerializeField] private Transform leftFistBone;
+    [SerializeField] private Transform rightLegUpperBone;
+    [SerializeField] private Transform rightLegLowerBone;
+    [SerializeField] private Transform rightFootBone;
+    [SerializeField] private Transform leftLegUpperBone;
+    [SerializeField] private Transform leftLegLowerBone;
+    [SerializeField] private Transform leftFootBone;
+
+    [Header("Executors")]
     [SerializeField] AnimationExecutor animationExecutor;
     [SerializeField] MovementExecutor movementExecutor;
+
+    [Header("Player Info")]
+    [SerializeField] private int playerId;
+
     private InputBuffer inputBuffer = new InputBuffer();
     private MovesManager movesManager = new MovesManager();
     private StateManager stateManager = new StateManager();
+    private CollisionBoxManager collisionBoxManager;
     private AnimationManager animationManager;
     private MovementManager movementManager;
     private MoveSelector moveSelector;
     private MoveExecutor moveExecutor;
 
-    public InputBuffer GetInputBuffer()
-    {
-        return inputBuffer;
-    }
-
-    public MoveSelector GetMoveSelector()
-    {
-        return moveSelector;
-    }
+    public int PlayerId => playerId;
 
     public void start()
     {
@@ -34,6 +50,9 @@ public class Player : MonoBehaviour
         animationManager.LoadAnimations();
         movesManager.LoadMoves(animationManager.GetAnimationDatabase());
         movementManager.LoadMovements();
+
+        collisionBoxManager = new CollisionBoxManager(stateManager);
+        collisionBoxManager.Load(body, GetBones());
     }
 
     public void update()
@@ -41,7 +60,77 @@ public class Player : MonoBehaviour
         moveSelector.Update();
         moveExecutor.Update();
         movementExecutor.update(moveExecutor.FrameCounter);
+        collisionBoxManager.Update();
         inputBuffer.UpdateFrameCounter();
         inputBuffer.RemoveExpiredInputs();
+    }
+
+    private Dictionary<string, Transform> GetBones()
+    {
+        return new Dictionary<string, Transform>
+        {
+            { "head", headBone },
+            { "body", bodyBone },
+            { "rightArmUpper", rightArmUpperBone },
+            { "rightArmLower", rightArmLowerBone },
+            { "rightFist", rightFistBone },
+            { "leftArmUpper", leftArmUpperBone },
+            { "leftArmLower", leftArmLowerBone },
+            { "leftFist", leftFistBone },
+            { "rightLegUpper", rightLegUpperBone },
+            { "rightLegLower", rightLegLowerBone },
+            { "rightFoot", rightFootBone },
+            { "leftLegUpper", leftLegUpperBone },
+            { "leftLegLower", leftLegLowerBone },
+            { "leftFoot", leftFootBone }
+        };
+    }
+
+    public List<CollisionBox> GetActiveHitboxes()
+    {
+        return collisionBoxManager.GetActiveHitboxes();
+    }
+
+    public IEnumerable<CollisionBox> GetAllHurtboxes()
+    {
+        return collisionBoxManager.GetAllHurtboxes();
+    }
+
+    public CollisionBox GetCollisionBox(string id)
+    {
+        return collisionBoxManager.GetCollisionBox(id);
+    }
+
+    public BodyCollider GetBodyCollider()
+    {
+        return collisionBoxManager.GetBodyCollider();
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;
+    }
+
+    public void SetMovementBlocked(bool blocked)
+    {
+        // movementExecutor.SetBlocked(blocked);
+    }
+
+    public InputBuffer GetInputBuffer()
+    {
+        return inputBuffer;
+    }
+
+    public MoveSelector GetMoveSelector()
+    {
+        return moveSelector;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (collisionBoxManager != null)
+        {
+            collisionBoxManager.OnDrawGizmos();
+        }
     }
 }
