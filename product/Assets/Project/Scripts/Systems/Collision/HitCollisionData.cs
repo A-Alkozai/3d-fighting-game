1c57f7b54124fa978695e10b9c38d8094fc6b889
Alkozai Ahad (2023) ZMAC392 <ahad.alkozai.2023@live.rhul.ac.uk>
2026-02-18 12:10:20 +0000

    Created class to store HitCollisionData properties from json #15

---
+++ product/Assets/Project/Scripts/Systems/Collision/HitCollisionData.cs
@@ -0,0 +1,16 @@
+public class HitCollisionData
+{
+    public ICollidable Attacker { get; }
+    public ICollidable Defender { get; }
+    public CollisionBox Hitbox { get; }
+    public CollisionBox Hurtbox { get; }
+
+    public HitCollisionData(ICollidable attacker, ICollidable defender,
+                            CollisionBox hitbox, CollisionBox hurtbox)
+    {
+        Attacker = attacker;
+        Defender = defender;
+        Hitbox = hitbox;
+        Hurtbox = hurtbox;
+    }
+}
