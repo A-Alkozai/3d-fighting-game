f112b9b9e4cc134096752873c653db788e3f012c
Alkozai Ahad (2023) ZMAC392 <ahad.alkozai.2023@live.rhul.ac.uk>
2026-04-04 12:45:30 +0100

    Updated collision interfaces and payloads to accurately track attack heights #18

--- product/Assets/Project/Scripts/Systems/Collision/HitCollisionData.cs
+++ product/Assets/Project/Scripts/Systems/Collision/HitCollisionData.cs
@@ -4,13 +4,16 @@ public class HitCollisionData
     public ICollidable Defender { get; }
     public CollisionBox Hitbox { get; }
     public CollisionBox Hurtbox { get; }
+    public CombatHitboxEntry HitboxEntry { get; }
 
     public HitCollisionData(ICollidable attacker, ICollidable defender,
-                            CollisionBox hitbox, CollisionBox hurtbox)
+                            CollisionBox hitbox, CollisionBox hurtbox,
+                            CombatHitboxEntry hitboxEntry)
     {
         Attacker = attacker;
         Defender = defender;
         Hitbox = hitbox;
         Hurtbox = hurtbox;
+        HitboxEntry = hitboxEntry;
     }
 }
